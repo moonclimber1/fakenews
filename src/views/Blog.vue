94554da7f594d09b654a7384cb39db4f87de520a
Jonas Wolter <jonas.wolter.ch@gmail.com>
2020-11-26 21:22:54 +0100

    Add blog

---
+++ src/views/Blog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="blog">
+    <h1>Webprojekt Fake News 3D</h1>
+    <p>
+      Unsere künstlerische Website richtet sich an gesellschaftspolitisch interessierte Menschen, die mehr über Fake News und
+      deren Hintergründe erfahren möchten. Die Website sensibilisiert und bietet eine visuell ansprechende 3D-Experience, in der
+      die Besucher*innen in das Rabbit Hole der Fake News hineingesogen werden, und kaum mehr hinauskommen.
+    </p>
+    <p>
+      Statt einer trockenen Fact-Checking Websites von dene es schon viele gibt, bietet unsere Seite eine interaktive und
+      multimediale Experience.
+    </p>
+    <br>
+    <img src="@/assets/escapingTheRabbitHole.jpg" alt="">
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.blog{
+  max-width: 600px;
+  margin: 0 auto;
+
+  img{
+    max-width: 80%;
+    margin: 0 auto;
+  }
+}
+</style>
