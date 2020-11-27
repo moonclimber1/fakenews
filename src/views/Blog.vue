63f9560792b52e771366fb061796b3a1535f9197
Jonas Wolter <jonas.wolter.ch@gmail.com>
2020-11-27 10:36:34 +0100

    Blog: Add Technologies

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,18 +2,30 @@
   <div class="blog">
     <div class="text">
       <h1>Webprojekt Fake News 3D</h1>
-    <p>
-      Unsere künstlerische Website richtet sich an gesellschaftspolitisch interessierte Menschen, die mehr über Fake News und
-      deren Hintergründe erfahren möchten. Die Website sensibilisiert und bietet eine visuell ansprechende 3D-Experience, in der
-      die Besucher*innen in das Rabbit Hole der Fake News hineingesogen werden, und kaum mehr hinauskommen.
-    </p>
-    <p>
-      Statt einer trockenen Fact-Checking Websites von dene es schon viele gibt, bietet unsere Seite eine interaktive und
-      multimediale Experience.
-    </p>
-    <br/>
-    <h2>Figma Moodboard</h2>
-   </div>
+      <p>
+        Unsere künstlerische Website richtet sich an gesellschaftspolitisch interessierte Menschen, die mehr über Fake News und
+        deren Hintergründe erfahren möchten. Die Website sensibilisiert und bietet eine visuell ansprechende 3D-Experience, in der
+        die Besucher*innen in das Rabbit Hole der Fake News hineingesogen werden, und kaum mehr hinauskommen.
+      </p>
+      <p>
+        Statt einer trockenen Fact-Checking Websites von dene es schon viele gibt, bietet unsere Seite eine interaktive und
+        multimediale Experience.
+      </p>
+
+      <br/>
+      <h2>Technologie</h2>
+      <p>
+        Three.js & CSS3DRenderer, Vue, evtl. GSAP
+      </p>
+      <p>
+        Wir möchten den Schritt ins Dreidimensionale wagen und mit dem CSS3DRenderer experimentieren. Eventuell könnte sich das
+        Thema auch noch von Fake News mehr zum Thema Informationsüberfluss verschieben.
+      </p>
+      <a href="https://threejs.org/examples/#css3d_youtube">Beispiel CSS3DRenderer</a>
+      <p></p>
+      <br/>
+      <h2>Figma Moodboard</h2>
+    </div>
     <iframe
       style="border: 1px solid rgba(0, 0, 0, 0.1);"
       width="1400"
@@ -21,6 +33,7 @@
       src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F5rtexo1EPpi6rWZcSVDR7x%2FWeb-Extended%3Fnode-id%3D0%253A1"
       allowfullscreen
     ></iframe>
+
     <!-- <img src="@/assets/escapingTheRabbitHole.jpg" alt="" /> -->
   </div>
 </template>
