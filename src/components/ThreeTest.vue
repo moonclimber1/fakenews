ec6a1c8314321bb737b9b56af937ef94507317a6
Jonas Wolter <jonas.wolter.ch@gmail.com>
2020-12-04 18:36:09 +0100

    Pyramid Mesh

--- src/components/ThreeTest.vue
+++ src/components/ThreeTest.vue
@@ -11,8 +11,10 @@ export default {
       camera: null,
       scene: null,
       renderer: null,
-      mesh: null,
+      videoMesh: null,
+      lineMesh: null,
       container: null,
+      lastTimestamp: null,
     };
   },
   props: {},
@@ -34,23 +36,46 @@ export default {
       //Resize Listener
       window.addEventListener('resize', this.onWindowResize, false);
     },
-    animate: function() {
-      requestAnimationFrame(this.animate);
-      this.mesh.rotation.x += 0.01;
-      this.mesh.rotation.y += 0.02;
-      this.renderer.render(this.scene, this.camera);
-    },
     createScene: function() {
       this.scene = new THREE.Scene();
       this.scene.background = new THREE.Color(0x000000);
       this.scene.fog = new THREE.FogExp2(0x000000, 0.4);
 
-      let geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-      let material = new THREE.MeshNormalMaterial();
+      // let geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 
-      this.mesh = new THREE.Mesh(geometry, material);
-      this.scene.add(this.mesh);
+      const geometry = new THREE.SphereBufferGeometry( 0.2, 4, 2);
+      const material = new THREE.MeshPhongMaterial({ color: 0xcccccc, flatShading: true})
+     
 
+      this.videoMesh = new THREE.Mesh(geometry, material);
+      this.scene.add(this.videoMesh);
+
+      const lineMeshGeometry = this.videoMesh.clone()
+      lineMeshGeometry.scale.set(1.5, 1.5, 1.5)
+      const edges = new THREE.EdgesGeometry( lineMeshGeometry.geometry );
+      this.lineMesh = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+      this.scene.add(this.lineMesh)
+
+      const light1 = new THREE.PointLight(0xFFFFFF, 1);
+      light1.position.set(3, 1, 1);
+      this.scene.add(light1);
+
+    },
+    animate: function(now) {
+      
+      requestAnimationFrame(this.animate);
+
+      // if(!this.lastTimestamp){this.lastTimestamp = now}
+      // let delta = (now - this.lastTimestamp) / 16.6667;
+      // this.lastTimestamp = now;
+      
+      this.videoMesh.rotation.x = now * 0.0005;
+      this.videoMesh.rotation.y = now * 0.0008;
+
+      this.lineMesh.rotation.x = now * 0.0005;
+      this.lineMesh.rotation.y = now * 0.0008;
+
+      this.renderer.render(this.scene, this.camera);
     },
     onWindowResize: function() {
       this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
