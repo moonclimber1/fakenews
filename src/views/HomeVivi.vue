c271144fb578b747b0586113eb7aba6ab0d15241
vivih <vivien.helbling@stud.hslu.ch>
2020-12-09 13:59:41 +0100

    Added Text Wall components & a view for vivi testing

---
+++ src/views/HomeVivi.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <moving-circle />
+     <three-test /> 
+    <text-layer/> 
+  </div>
+</template>
+
+<script>
+import ThreeTest from "../components/ThreeTest.vue";
+import ThreeTestVivi from "../components/ThreeTestVivi.vue";
+import MovingCircle from "../components/MovingCircle";
+import TextLayer from '../components/TextLayer.vue';
+
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {
+    ThreeTest,
+    MovingCircle,
+    TextLayer,
+  },
+};
+</script>
+
+<style lang="scss"></style>
