b616bcc95a52ae628fbb7ca5cf16590c1ffba94c
Jonas Wolter <jonas.wolter.ch@gmail.com>
2020-12-11 06:26:25 +0100

    Random Video Selection

--- src/components/FakeNewsGalaxy.vue
+++ src/components/FakeNewsGalaxy.vue
@@ -1,9 +1,39 @@
 <template>
   <div id="canvas-wrapper"></div>
-  <video id="video" loop muted crossOrigin="anonymous" playsinline style="display:none">
-    <source src="@/assets/sintel.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-    <text-layer />
+  <video id="video1" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_1.mp4" type="video/mp4" />
   </video>
+  <video id="video2" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_2.mp4" type="video/mp4" />
+  </video>
+  <video id="video3" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_3.mp4" type="video/mp4" />
+  </video>
+  <video id="video4" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_4.mp4" type="video/mp4" />
+  </video>
+  <video id="video5" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_5.mp4" type="video/mp4" />
+  </video>
+  <video id="video6" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_6.mp4" type="video/mp4" />
+  </video>
+  <video id="video7" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_7.mp4" type="video/mp4" />
+  </video>
+  <video id="video8" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_8.mp4" type="video/mp4" />
+  </video>
+  <video id="video9" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_9.mp4" type="video/mp4" />
+  </video>
+  <video id="video10" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_10.mp4" type="video/mp4" />
+  </video>
+  <video id="video11" loop crossOrigin="anonymous" playsinline style="display:none">
+    <source src="@/assets/videos/clip_11.mp4" type="video/mp4" />
+  </video>
+  <text-layer />
 </template>
 
 <script>
@@ -16,7 +46,7 @@ import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer
 import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
 import { MeshLine, MeshLineMaterial, MeshLineRaycast } from "three.meshline";
 import TextLayer from "./TextLayer.vue";
-import { log, MeshBasicMaterial, Object3D } from "three";
+import { log, MathUtils, MeshBasicMaterial, Object3D, Vector3 } from "three";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -35,7 +65,9 @@ export default {
       sceneCSS: {},
       rendererCSS: {},
       cameraAnimationPath: {},
-      cameraTween: {}
+      cameraTween: {},
+      cursor: {},
+      tilt: {},
     };
   },
   props: {},
@@ -89,22 +121,20 @@ export default {
       // Setup stats
       this.stats = new Stats();
       document.body.appendChild(this.stats.dom);
+
+      this.cursor = { x: 0, y: 0 };
+      const self = this;
+      window.addEventListener("mousemove", function(event) {
+        self.cursor.x = event.clientX;
+        self.cursor.y = event.clientY;
+      });
     },
     createScene: function() {
       this.sceneGL = new THREE.Scene();
       this.sceneCSS = new THREE.Scene();
 
-      // const diamond = this.createVideoDiamond()
-      // this.sceneGL.add(diamond)
-
-      // const cube = this.createVideoCube();
-      // this.sceneGL.add(cube);
-
       const spiral = this.createSpiral();
       this.sceneGL.add(spiral);
-
-      // const cubeSphere = this.createRandomCubesInSphere(0.5,30)
-      // this.sceneGL.add(cubeSphere)
 
       // CSS 3D Object
       const textLayer = document.getElementById("textLayer");
@@ -114,41 +144,65 @@ export default {
       object.scale.set(0.001, 0.001, 0.001);
       this.sceneCSS.add(object);
 
-      const pointArray = [new THREE.Vector3(5.5,-3.5,9),...this.getTheodorus(120, 0.6)]
-      this.cameraAnimationPath =  new THREE.CatmullRomCurve3(pointArray)
+      const theodorus = this.getTheodorus(120, 0.6);
+      theodorus.shift();
+      const pointArray = [new THREE.Vector3(5.5, -3.5, 9), ...theodorus];
+      this.cameraAnimationPath = new THREE.CatmullRomCurve3(pointArray);
       const geometry = new THREE.TubeGeometry(this.cameraAnimationPath, 1024, 0.01, 12, false);
       const material = new MeshBasicMaterial();
       var tube = new THREE.Mesh(geometry, material);
       // this.sceneGL.add(tube)
 
-
-      this.cameraTween = {val: 1} 
+      this.cameraTween = { val: 1 };
 
       gsap.registerPlugin(ScrollTrigger);
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: "#canvas-wrapper",
           start: "top top",
-          end: function (){ return '+='+window.innerHeight*15 + ' bottom'},
+          end: function() {
+            return "+=" + window.innerHeight * 15 + " bottom";
+          },
           toggleActions: "restart pause reverse pause",
           // markers: true,
           scrub: 1,
           pin: true,
         },
-      })
-      
-      tl.from(this.cameraTween, {duration: 100, val: 0 })
-    
+      });
+
+      tl.from(this.cameraTween, { duration: 100, val: 0 });
+
+      const listener = new THREE.AudioListener();
+      this.camera.add(listener);
+
+      // create the PositionalAudio object (passing in the listener)
+      const sound = new THREE.PositionalAudio(listener);
+
+      // load a sound and set it as the PositionalAudio object's buffer
+      const audioLoader = new THREE.AudioLoader();
+      audioLoader.load("../assets/music.mp3", function(buffer) {
+        console.log("🚀 ~ file: FakeNewsGalaxy.vue ~ line 154 ~ audioLoader.load ~ buffer", buffer);
+        sound.setBuffer(buffer);
+        sound.setRefDistance(20);
+        sound.play();
+      });
+      // console.log("🚀 ~ file: FakeNewsGalaxy.vue ~ line 157 ~ sound", sound)
+      spiral.add(sound);
     },
     createVideoDiamond: function() {
       const diamond = new THREE.Object3D();
 
       // Create inner Video Mesh
       const geometry = new THREE.SphereBufferGeometry(0.2, 4, 2);
-      const material = this.createVideoMaterial("video");
+      const material = this.createVideoMaterial(this.getRandomVideoId());
       // const material = new THREE.MeshPhongMaterial({ color: 0xcccccc, flatShading: true });
       const videoMesh = new THREE.Mesh(geometry, material);
-      videoMesh.rotation.y = -Math.PI / 2;
+
+      // videoMesh.rotation.x = this.deg2rad(90)
+
+      // videoMesh.rotation.y = this.deg2rad(90)
+      videoMesh.rotation.x = this.deg2rad(90);
+      videoMesh.rotation.y = this.deg2rad(180);
 
       diamond.add(videoMesh);
       diamond.add(this.createEdgesMesh(geometry));
@@ -164,12 +218,18 @@ export default {
     createVideoCube: function() {
       const cube = new THREE.Object3D();
       const geometry = new THREE.BoxBufferGeometry(0.2, 0.2, 0.2);
-      const material = this.createVideoMaterial("video");
+      const material = this.createVideoMaterial(this.getRandomVideoId());
       const videoMesh = new THREE.Mesh(geometry, material);
       cube.add(videoMesh);
       cube.add(this.createEdgesMesh(geometry));
 
+      cube.rotation.y = this.deg2rad(90);
+      cube.rotation.z = this.deg2rad(90);
       return cube;
+    },
+    getRandomVideoId(){
+      const videoNr = Math.floor(Math.random() * 11)+1
+      return "video"+videoNr
     },
     createVideoMaterial(id) {
       const video = document.getElementById(id);
@@ -223,19 +283,26 @@ export default {
         const spiralPoint = new THREE.Object3D();
         spiralPoint.position.set(point.x, point.y, point.z);
 
-        if (index % 3 == Math.round(Math.random() - 0.3)) {
-          const diamond = this.createVideoDiamond();
-          diamond.position.set((Math.random() - 0.5) * 0.5, (Math.random() - 0.5) * 0.5, (Math.random() - 0.5) * 1.0);
-          spiralPoint.add(diamond);
+        if (index >= 0 && index < points.length - 1) {
+          const nextPoint = points[index + 1];
+
+          // spiralPoint.lookAt(nextPoint)
+          spiralPoint.up.set(0, 0, 1);
         }
 
-        if (index % 7 == Math.round(Math.random()-0.3)) {
+        if (index % 5 == Math.round(Math.random() - 0.3)) {
           const cube = this.createVideoCube();
           cube.position.set((Math.random() - 0.5) * 0.7, (Math.random() - 0.5) * 0.7, (Math.random() - 0.5) * 1.2);
           spiralPoint.add(cube);
         }
 
-        const cubeSphere = this.createRandomCubesInSphere(0.5, 15);
+        if (index % 8 == Math.round(Math.random() - 0.3)) {
+          const diamond = this.createVideoDiamond();
+          diamond.position.set((Math.random() - 0.5) * 0.5, (Math.random() - 0.5) * 0.5, (Math.random() - 0.5) * 1.0);
+          spiralPoint.add(diamond);
+        }
+
+        const cubeSphere = this.createRandomCubesInSphere(0.5, 12);
 
         spiralPoint.add(cubeSphere);
         spiral.add(spiralPoint);
@@ -260,27 +327,56 @@ export default {
     animate: function(now) {
       requestAnimationFrame(this.animate);
 
-      var p1 = this.cameraAnimationPath.getPointAt(this.cameraTween.val);
-      var p2 = this.cameraAnimationPath.getPointAt(this.cameraTween.val + 0.0001);
+      const p1 = this.cameraAnimationPath.getPointAt(this.cameraTween.val);
+      const p2 = this.cameraAnimationPath.getPointAt(this.cameraTween.val + 0.0001);
       this.camera.position.set(p1.x, p1.y, p1.z);
       this.camera.lookAt(p2);
       this.camera.up.set(0, 0, 1);
+
+      const desiredYTilt = ((this.cursor.x / window.innerWidth) * 2 - 1) * this.deg2rad(4);
+      const yTilt = THREE.MathUtils.lerp(this.sceneGL.rotation.y, desiredYTilt, 0.04);
+      this.sceneGL.rotation.y = yTilt;
+      this.sceneCSS.rotation.y = yTilt;
+
+      const desiredXTilt = ((this.cursor.y / window.innerHeight) * 2 - 1) * this.deg2rad(3);
+      const xTilt = THREE.MathUtils.lerp(this.sceneGL.rotation.x, desiredXTilt, 0.04);
+      this.sceneGL.rotation.x = xTilt;
+      this.sceneCSS.rotation.x = xTilt;
+
+      // const up = new Vector3(0,0,1)
+      // const desiredYTilt = ((this.cursor.x / window.innerWidth) * 2 - 1) * this.deg2rad(2);
+      // // const desiredTiltVec = new Object3D().rotateOnWorldAxis(p2, yTilt)
+      // // console.log("🚀 ~ file: FakeNewsGalaxy.vue ~ line 311 ~ desiredYTilt", desiredYTilt)
+      // const yTilt = THREE.MathUtils.lerp(this.sceneGL.rotation.y, desiredYTilt, 0.04);
+      // this.sceneGL.rotation.set(0,0,0)
+      // this.sceneCSS.rotation.set(0,0,0)
+      // this.sceneGL.rotateOnAxis(this.camera.up, desiredYTilt);
+      // this.sceneCSS.rotateOnAxis(this.camera.up, desiredYTilt);
+
+      // this.tilt = {lastLookAtTilt: desiredYTilt}
+
+      // const desiredXTilt = ((this.cursor.y / window.innerHeight) * 2 - 1) * this.deg2rad(5);
+      // const xTilt = THREE.MathUtils.lerp(this.sceneGL.rotation.x, desiredXTilt, 0.04);
+      // this.sceneGL.rotation.x = xTilt;
+      // this.sceneCSS.rotation.x = xTilt;
 
       // this.controls.update();
       this.composerGL.render();
       this.rendererCSS.render(this.sceneCSS, this.camera);
       this.stats.update();
-      
-      // console.log("🚀 ~ file: FakeNewsGalaxy.vue ~ line 254 ~ this.camera.position", this.camera.position)
 
-      
+      // console.log("🚀 ~ file: FakeNewsGalaxy.vue ~ line 254 ~ this.camera.position", this.camera.position)
     },
+
     onWindowResize: function() {
       this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
       this.camera.updateProjectionMatrix();
       this.rendererGL.setSize(this.container.clientWidth, this.container.clientHeight);
       this.composerGL.setSize(this.container.clientWidth, this.container.clientHeight);
       this.rendererCSS.setSize(this.container.clientWidth, this.container.clientHeight);
+    },
+    deg2rad(deg) {
+      return deg * (Math.PI / 180);
     },
   },
   mounted() {
